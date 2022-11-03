92685c3802ce88f74963b67492aa2ebd7cdf0aec
lordscoba <lordscoba2tm@gmail.com>
2022-11-03 18:48:23 +0100

    Added ReadMe file

---
+++ README.md
@@ -0,0 +1,29 @@
+## CSV TO JSON CONVERTER USING CHIP-007 FORMAT
+
+<br>
+
+😄 <h3><strong>This Project was built with Golang </strong></h3>
+
+⚡ I made the documentation as simple as possible
+
+<ul>
+<li>First of all you have to make sure you have Golang installed in your computer</li>
+<li>visit to install in Windows https://linuxhint.com/install-use-go-ubuntu-22-04/</li>
+<li>visit to install in Linux and MacOS https://go.dev/doc/install</li>
+<br>
+<br>
+<li>Clone the file from the github repository on your CLI using</li>
+<code>$git clone https://github.com/lordscoba/hng-csv-generator.git</code>
+<code>$git status </code>
+
+<li>Open your computer terminal and use the cd command to enter the  <strong>hng-csv-generator/</strong>  folder</li>
+<li>Run the following code</li>
+<code>$go mod init github.com/lordscoba/hng-csv-generator</code>
+<li>Put the CSV file you want to use in the <strong>hng-csv-generator/</strong> root directory</li>
+<li>Put the File name (filename.csv) and extention inside the quotation on line 60</li>
+<li>Run the following code</li>
+<code>$go build</code>
+<code>$go run main.go</code>
+</ul>
+
+## Thank you for stoping by 👋
--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
+// Importation of required files
 import (
 	"bytes"
 	"encoding/csv"
- "encoding/json"
- "fmt"
- "os"
- "crypto/sha256"
- "log"
+	"encoding/json"
+	"fmt"
+	"os"
+	"crypto/sha256"
+	"log"
 )
+
+
+//Initialization of NftRecord variables
 type NftRecord struct{
   Format        string  `json:"format"`
   Name          string  `json:"name"`
@@ -22,57 +26,70 @@ type NftRecord struct{
   Data    Data  `json:"data"`
 }
 
+
+// Initialization of attributes variables
 type Attributes struct{
   TraitType string  `json:"trait_type"`
   Value     string  `json:"value"`
 }
 
+
+// initialization of collection variables
 type Collection struct{
   Name  string  `json:"name"`
   Id    string  `json:"id"`
   Attributes1    []Attributes1 `json:"attributes1"`
 }
 
+// initialization of attributes of variables
 type Attributes1 struct{
   Type string  `json:"type"`
   Value     string  `json:"value"`
 }
 
+
+// initialization of Data variables
 type Data  struct{
   ExampleData string `json:"example_data"`
 }
 
 
+// where the main code is written
 func main() {
-	csvFile, err := os.Open("./nft-data.csv")
-	if err != nil {
+
+	// put the file location in this variable
+	workFile := "./nft-data.csv"
+
+		// here we open the csv file containing the nfts
+		csvFile, err := os.Open(workFile)
+		if err != nil {
 		fmt.Println(err)
-	}
-	defer csvFile.Close()
+		}
+		defer csvFile.Close()
 
-	reader := csv.NewReader(csvFile)
-	reader.FieldsPerRecord = -2
-
-	csvData, err := reader.ReadAll()
-	if err != nil {
+		// here we read the csv file containing the nfts
+		reader := csv.NewReader(csvFile)
+		reader.FieldsPerRecord = -2
+		csvData, err := reader.ReadAll()
+		if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
-	}
+		}
 
+// we use this if we want to create one file
 	// var nft1 []NftRecord
 
+// here we iterate to create the csv files
 	for _, each := range csvData {
-		// nft.Format = "CHIP-0007"
-		// nft.Number = each[0]
-		// nft.Filename = each[1]
-		// nft.UUID = each[2]
-		// nft.Data = "some string"
-		//
-		//
+
+// here we skip the header to read the rest of the files
 		if each[1] == "Filename" {
          // skip header line
          continue
       }
+
+
+	// here we assign values to the nftRecords	 struct
 		var nftRecord NftRecord
 		nftRecord.Format = "CHIP-0007"
 		nftRecord.Name = each[1]
@@ -83,10 +100,11 @@ func main() {
 		nftRecord.SeriesTotal = each[2]
 
 
+//here we assign value to Collection record struct
 		nftRecord.Collection.Name = "Example Pokémon Collection"
 		nftRecord.Collection.Id = "e43fcfe6-1d5c-4d6e-82da-5de3aa8b3b57"
 
-
+// here we append values to the attributes struct
 		var attributes Attributes
 		attributes.TraitType = "Color"
 		attributes.Value = "Yellow"
@@ -95,6 +113,7 @@ func main() {
 		attributes.Value = "Yellow1"
 		nftRecord.Attributes = append(nftRecord.Attributes, attributes)
 
+// here we append values to the 2nd attributes struct
 		var attributes1 Attributes1
 		attributes1.Type = "description"
 		attributes1.Value = "Example Pokémon Collection is the best Pokémon collection. Get yours today!"
@@ -103,51 +122,56 @@ func main() {
 		attributes1.Value = "Example Pokémon Collection is the best Pokémon collection. Get yours todaywe!"
 		nftRecord.Collection.Attributes1 = append(nftRecord.Collection.Attributes1, attributes1)
 
+//Here we read the read the whole json for  hashing
 		reqBodyBytes := new(bytes.Buffer)
 		json.NewEncoder(reqBodyBytes).Encode(nftRecord)
 
+// here we hash using hash256
 		// fmt.Println(reqBodyBytes.Bytes()) // this is the []byte
 		h := sha256.New()
 		h.Write(reqBodyBytes.Bytes())
-
 		bs := h.Sum(nil)
-
 		tt := fmt.Sprintf("%x", bs)
 		// fmt.Println("\n")
 		// fmt.Println(tt)
-nftRecord.Data.ExampleData = tt
+		nftRecord.Data.ExampleData = tt
 
 
-// Convert to JSON
-jsonData, err := json.Marshal(nftRecord)
-if err != nil {
-	fmt.Println(err)
-	os.Exit(1)
-}
-
-fmt.Println(string(jsonData))
-
-path := "output"
-err = os.Mkdir(path, os.ModePerm)
+		 // here we convert the whole script to json
+		jsonData, err := json.Marshal(nftRecord)
 		if err != nil {
-			log.Println(err)
+			fmt.Println(err)
+			os.Exit(1)
 		}
-jsonFile, err := os.Create("./output/"+each[1]+".json")
-if err != nil {
-	fmt.Println(err)
-}
-defer jsonFile.Close()
 
-jsonFile.Write(jsonData)
-jsonFile.Close()
+// here i print the json files on my console
+		fmt.Println(string(jsonData))
 
+// here I create the output folder
+		path := "output"
+		err = os.Mkdir(path, os.ModePerm)
+				if err != nil {
+					log.Println(err)
+				}
 
+// here I create the json files needed in the output folder
+		jsonFile, err := os.Create("./output/"+each[1]+".json")
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer jsonFile.Close()
+
+// here I write the json data for the created files
+		jsonFile.Write(jsonData)
+		jsonFile.Close()
+
+// here i break the code if the iteration meets an empty space
 		if each[1] == "" {
          // skip header line
          break
       }
 
-// if you want to create one file
+// if you want to create one file and append all
 		// nft1 = append(nft1, nftRecord)
 
 	}
